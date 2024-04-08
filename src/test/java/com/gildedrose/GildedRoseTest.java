8ed6254cde022f889bf6c0f5f9925befa0343a69
le2sky <leehaneul990623@gmail.com>
2024-04-08 18:57:07 +0900

    feat: 1단계 완성

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,16 +1,32 @@
 package com.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Arrays;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        String[] name = {
+                "foo",
+                "Aged Brie",
+                "Sulfuras, Hand of Ragnaros",
+                "Backstage passes to a TAFKAL80ETC concert"
+        };
+        Integer[] sellIn = {-1, 0, 1, 6, 11};
+        Integer[] quality = {0, 1, 49, 50};
+
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, name, sellIn, quality);
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{
+                new Item(name, sellIn, quality)
+        };
+        GildedRose sut = new GildedRose(items);
+        sut.updateQuality();
+
+        return Arrays.asList(sut.items).toString();
     }
 }
