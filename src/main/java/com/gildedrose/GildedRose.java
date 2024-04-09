90bcf00812cbef036226e1f15ebfccc389dec55f
le2sky <leehaneul990623@gmail.com>
2024-04-09 14:03:33 +0900

    refactor: 다형성을 위해 switch 문으로 변경

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,10 +15,10 @@ class GildedRose {
     }
 
     private void updateQualityFor(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-        } else {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        switch (item.name) {
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -36,29 +36,29 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            } else {
-                if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0 && item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                break;
+            case "Aged Brie":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
-            }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0 && item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0 && item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                break;
         }
     }
 }
