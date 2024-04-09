2b96c78ea4b3dd8f471f82dc8eab05fd07a7e248
le2sky <leehaneul990623@gmail.com>
2024-04-09 14:05:58 +0900

    refactor: 메서드를 item 내부로 이동

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,55 +10,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateQualityFor(item);
-        }
-    }
-
-    private void updateQualityFor(Item item) {
-        switch (item.name) {
-            case "Sulfuras, Hand of Ragnaros":
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-
-                    if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-                break;
-            case "Aged Brie":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-                break;
-            default:
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0 && item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-                break;
+            item.updateQualityFor();
         }
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -12,6 +12,54 @@ class Item {
         this.quality = quality;
     }
 
+    void updateQualityFor() {
+        switch (name) {
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
+
+                    if (sellIn < 11 && quality < 50) {
+                        quality = quality + 1;
+                    }
+
+                    if (sellIn < 6 && quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0 && quality < 50) {
+                    quality = quality + 1;
+                }
+                break;
+            default:
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0 && quality > 0) {
+                    quality = quality - 1;
+                }
+                break;
+        }
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
