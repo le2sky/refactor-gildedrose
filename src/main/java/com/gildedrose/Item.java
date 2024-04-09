79e8ddef791b7f26e1f31f7a971a4ea74cb1467f
le2sky <leehaneul990623@gmail.com>
2024-04-09 14:28:46 +0900

    refactor: 미사용 코드 정리

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -25,51 +25,15 @@ class Item {
         }
     }
 
-    void updateQualityFor() {
-        switch (name) {
-            case "Sulfuras, Hand of Ragnaros":
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (quality < 50) {
-                    quality = quality + 1;
+    void updateQuality() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
 
-                    if (sellIn < 11 && quality < 50) {
-                        quality = quality + 1;
-                    }
+        sellIn = sellIn - 1;
 
-                    if (sellIn < 6 && quality < 50) {
-                        quality = quality + 1;
-                    }
-                }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0) {
-                    quality = 0;
-                }
-                break;
-            case "Aged Brie":
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0 && quality < 50) {
-                    quality = quality + 1;
-                }
-                break;
-            default:
-                if (quality > 0) {
-                    quality = quality - 1;
-                }
-
-                sellIn = sellIn - 1;
-
-                if (sellIn < 0 && quality > 0) {
-                    quality = quality - 1;
-                }
-                break;
+        if (sellIn < 0 && quality > 0) {
+            quality = quality - 1;
         }
     }
 
